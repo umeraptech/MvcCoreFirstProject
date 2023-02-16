2c572a3dd9e72f2cd09344fcb034fe5d4f83ae5c
umeraptech <umer@aptechgdn.net>
2023-02-16 20:20:59 +0500

    Project created first example

---
+++ Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreFirstProject.Models
+{
+    public class Person
+    {
+        
+        public int ID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class PersonContext : DbContext {
+        public PersonContext(DbContextOptions<PersonContext> options) : base(options) {
+            
+        }
+        public DbSet<Person> people { get; set; }
+    }
+       
+}
